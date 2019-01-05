aa6a2b312424ac76b95edda22cd76f88e1173ecd
Dennis Walters <pooster@gmail.com>
2019-01-05 00:45:59 -0500

    Added yard documentation for most stuff

--- lib/sekrat/crypter/base.rb
+++ lib/sekrat/crypter/base.rb
@@ -2,11 +2,22 @@ require 'sekrat/errors'
 
 module Sekrat
   module Crypter
+
+    # A base mixin for would-be Sekrat::Crypter implementations
     module Base
+
+      # A stub for incomplete warehouse implementations
+      # @param key [String] a key that will never get used
+      # @param data [String] some data that will never get used
+      # @raise [Sekrat::NotImplemented]
       def encrypt(key, data)
         raise NotImplemented.new("encrypt is not implemented")
       end
 
+      # A stub for incomplete warehouse implementations
+      # @param key [String] a key that will never get used
+      # @param data [String] some data that will never get used
+      # @raise [Sekrat::NotImplemented]
       def decrypt(key, data)
         raise NotImplemented.new("decrypt is not implemented")
       end
--- lib/sekrat/crypter/passthrough.rb
+++ lib/sekrat/crypter/passthrough.rb
@@ -2,13 +2,23 @@ require 'sekrat/crypter/base'
 
 module Sekrat
   module Crypter
+
+    # A crypter that doesn't actually do any crypting
     class Passthrough
       include Base
 
+      # Given a key and some data, use that key to encrypt the data.
+      # @param key [String] the encryption key to use
+      # @param data [String] the data to encrypt
+      # @return [String] the original data
       def encrypt(key, data)
         data
       end
 
+      # Given a key and some data, use that key to decrypt the data.
+      # @param key [String] the encryption key to use
+      # @param data [String] the data to decrypt
+      # @return [String] the decrypted data
       def decrypt(key, data)
         data
       end
